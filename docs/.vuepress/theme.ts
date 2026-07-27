5bb64b720e9258bdbd7677a5bb032846e491a929
AndersonHJB <AndersonHJB@users.noreply.github.com>
2026-07-27 17:37:09 +0800

    添加 docsearch 插件并移除冗余的搜索配置

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -268,67 +268,60 @@ const theme = hopeTheme({
     //   }
     // },
     // slimsearch: true,
-    search: {
-      maxSuggestions: 10,
-      hotKeys: [],
-      // 你的选项
-      locales: {
-        "/": {
-          placeholder: "搜索",
-        }
-      }
-    },
-    // docsearch: {
+    // search: {
+    //   maxSuggestions: 10,
+    //   hotKeys: [],
     //   // 你的选项
-    //   // appId, apiKey 和 indexName 是必填的
-    //   appId: "ACD1S100IT",
-    //   apiKey: "409311158985cfee5c0a535e5998ddd2",
-    //   indexName: "bornforthis",
-    //   // placeholder: "搜索",
     //   locales: {
     //     "/": {
-    //       placeholder: "搜索文档",
-    //       translations: {
-    //         button: {
-    //           buttonText: "搜索文档",
-    //           buttonAriaLabel: "搜索文档",
-    //         },
-    //         modal: {
-    //           searchBox: {
-    //             resetButtonTitle: "清除查询条件",
-    //             resetButtonAriaLabel: "清除查询条件",
-    //             cancelButtonText: "取消",
-    //             cancelButtonAriaLabel: "取消",
-    //           },
-    //           startScreen: {
-    //             recentSearchesTitle: "搜索历史",
-    //             noRecentSearchesText: "没有搜索历史",
-    //             saveRecentSearchButtonTitle: "保存至搜索历史",
-    //             removeRecentSearchButtonTitle: "从搜索历史中移除",
-    //             favoriteSearchesTitle: "收藏",
-    //             removeFavoriteSearchButtonTitle: "从收藏中移除",
-    //           },
-    //           errorScreen: {
-    //             titleText: "无法获取结果",
-    //             helpText: "你可能需要检查你的网络连接",
-    //           },
-    //           footer: {
-    //             selectText: "选择",
-    //             navigateText: "切换",
-    //             closeText: "关闭",
-    //             searchByText: "搜索提供者",
-    //           },
-    //           noResultsScreen: {
-    //             noResultsText: "无法找到相关结果",
-    //             suggestedQueryText: "你可以尝试查询",
-    //             reportMissingResultsText: "你认为该查询应该有结果？",
-    //             reportMissingResultsLinkText: "点击反馈",
-    //           },
-    //         },
-    //       },
-    //     },
-    //   },
+    //       placeholder: "搜索",
+    //     }
+    //   }
     // },
+    docsearch: {
+      // 你的选项
+      // appId, apiKey 和 indexName 是必填的
+      appId: "ACD1S100IT",
+      apiKey: "409311158985cfee5c0a535e5998ddd2",
+      indexName: "bornforthis",
+      // placeholder: "搜索",
+      locales: {
+        "/": {
+          placeholder: '搜索文档',
+          translations: {
+            button: {
+              buttonText: '搜索文档',
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
+          },
+        },
+      },
+    },
 
     copyCode: {
       showInMobile: true,
